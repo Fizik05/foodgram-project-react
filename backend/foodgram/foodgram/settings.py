e4a335fe28d21cd64c44ad0499d171e3727ad60a
Fizik05 <bboybaga13@mail.ru>
2022-04-17 20:56:40 +0300

    My third commit1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -73,10 +73,10 @@ DATABASES = {
         "ENGINE": os.getenv(
             "DB_ENGINE", default="django.db.backends.postgresql"
         ),
-        "NAME": os.getenv("DB_NAME", default="postgres"),
+        "NAME": os.getenv("DB_NAME", default="foodgrampython:v2.0"),
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="14012005"),
-        "HOST": os.getenv("DB_HOST", default="0.0.0.0"),
+        "HOST": os.getenv("DB_HOST", default="127.0.0.1"),
         "PORT": os.getenv("DB_PORT", default="5432"),
     }
 }
