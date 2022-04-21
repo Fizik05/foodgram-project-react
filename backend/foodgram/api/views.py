9c35312da5f6b5e78a0be6c2bc99421b3a0334f7
Fizik05 <bboybaga13@mail.ru>
2022-04-22 00:03:45 +0300

    My seventh commit6

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -83,15 +83,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 }
             else:
                 final_list[name]['amount'] += item[2]
-        #pdfmetrics.registerFont(
-        #    TTFont('FreeSans', 'media/fonts/FreeSans.ttf'))
+        pdfmetrics.registerFont(
+            TTFont('FreeSans', 'FreeSans.ttf'))
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = ('attachment; '
                                            'filename="shopping_list.pdf"')
         page = Canvas(response)
-        #page.setFont('FreeSans', size=24)
+        page.setFont('FreeSans', size=24)
         page.drawString(200, 800, 'Список ингредиентов')
-        #page.setFont('FreeSans', size=16)
+        page.setFont('FreeSans', size=16)
         height = 750
         for i, (name, data) in enumerate(final_list.items(), start=1):
             page.drawString(75, height, (f'{i}) {name} - {data["amount"]} '
